f03028044f59f630ac22eff28dcfd7050e2ec8b4
buloz <lbizeray30@gmail.com>
2024-04-13 12:51:31 +0200

    [config] ignore addons folder

--- Projet/Scenes/Component/hitboxComponent.gd
+++ Projet/Scenes/Component/hitboxComponent.gd
@@ -2,10 +2,6 @@ class_name HitboxComponent extends Area2D
 
 @export var healthComponent: HealthComponent
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
 func damage(attack: AttackComponent):
 	if healthComponent:
 		healthComponent.damage(attack)
