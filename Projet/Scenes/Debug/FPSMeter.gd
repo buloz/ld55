b762db43c8107158028f0d1fa682628089fbcefd
obani <obani@pi-et-ro.net>
2024-04-15 14:03:12 +0200

    Fix debug panel

--- Projet/Scenes/Debug/FPSMeter.gd
+++ Projet/Scenes/Debug/FPSMeter.gd
@@ -1,9 +1,8 @@
 extends Label
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	if !OS.has_feature("editor"):
+		get_parent().queue_free()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
