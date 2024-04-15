978683bfec455b346ae04e81a099628f6b08d964
obani <obani@pi-et-ro.net>
2024-04-15 19:18:07 +0200

    Add some attack sounds

--- Projet/Scenes/Attacks/AttackAnimation.gd
+++ Projet/Scenes/Attacks/AttackAnimation.gd
@@ -4,20 +4,25 @@ extends Sprite2D
 
 
 func indexToAnimation():
-	
 	match animationIndex:
 		0:
 			$AnimationPlayer.play("fireBall")
+			$fireball.play()
 		1:
 			$AnimationPlayer.play("cailloux")
+			$fireball.play()
 		2:
 			$AnimationPlayer.play("flaque")
+			$flaque.play()
 		3:
 			$AnimationPlayer.play("melee")
+			$melee.play()
 		4:
 			$AnimationPlayer.play("bomb")
+			$fireball.play()
 		5:
 			$AnimationPlayer.play("fire")
+			$fireball.play()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
