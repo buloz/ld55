eea1d8860c1c8507b985cf651d1e68ac04e6be61
obani <obani@pi-et-ro.net>
2024-04-15 22:25:30 +0200

    Retry button works properly now

--- Projet/Scenes/Global/Music.gd
+++ Projet/Scenes/Global/Music.gd
@@ -1,8 +1,8 @@
 extends Node
 
-const MIN_DB := -50.0
+const MIN_DB := -40.0
 const MAX_DB := -10.0
-const TRANS = 2.0
+const TRANS = 1.0
 
 var current_music = null
 var tween = null
@@ -14,15 +14,17 @@ func play(n : String):
 	
 	var former_music = current_music
 	current_music = get_node(n)
-	current_music.play()
 
-	if former_music != null:
+	if former_music != null and former_music != current_music:
 		if tween: tween.kill()
 		
 		tween = create_tween()
 		tween.set_ease(Tween.EASE_IN_OUT)
 		tween.set_trans(Tween.TRANS_SINE)
 		
+		current_music.volume_db = MIN_DB
 		tween.tween_property(former_music, "volume_db", MIN_DB, TRANS)
 		tween.chain().tween_property(current_music, "volume_db", MAX_DB, TRANS)
 		tween.tween_callback(former_music.stop)
+	
+	current_music.play()
